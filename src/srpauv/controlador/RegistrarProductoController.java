21ac4b6eda933e84ac0c542a15d767e0d7159189
Rodrigo Ordóñez <rodrigo.op1791@gmail.com>
2019-04-02 01:02:16 -0600

    Métodos creación de productos: AI, AA, A, PI y ME

--- src/srpauv/controlador/RegistrarProductoController.java
+++ src/srpauv/controlador/RegistrarProductoController.java
@@ -7,14 +7,14 @@ package srpauv.controlador;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import srpauv.clases.ArticuloIndexado;
-import srpauv.clases.Tesis;
+import srpauv.clases.*;
 
 /**
  * FXML Controller class
@@ -117,7 +117,7 @@ public class RegistrarProductoController implements Initializable {
     //PRODUCCION INNOVADORA-----------------------------------------------------
     @FXML TextField txttituloprod;
     @FXML TextField txtparticipanteprod;
-    @FXML TextField txtclasfiprod;
+    @FXML TextField txtclasifprod;
     @FXML TextField txtnumregprod;
     @FXML TextField txtpaisprod;
     @FXML TextField txtpropositoprod;
@@ -362,6 +362,10 @@ public class RegistrarProductoController implements Initializable {
         }
     }
     
+    /**
+     * Metódo que recopila la información de los campos en la GUI para la creación
+     * de un objeto TESIS
+     */
     private void guardarTesis(){
         Tesis tesis = new Tesis();
         String titulo = txtTituloTesis.getText();
@@ -392,6 +396,11 @@ public class RegistrarProductoController implements Initializable {
             }
         }
     }
+    
+    /**
+     * Metódo que recopila la información de los campos en la GUI para la creación
+     * de un objeto TESIS
+     */
     private void guardarArticuloIndexado(){
         /**
          * Recopilación de datos de campos de texto
@@ -431,12 +440,206 @@ public class RegistrarProductoController implements Initializable {
                     articuloindexado.setPags(pags);
                     articuloindexado.setPais(pais);
                     articuloindexado.setAno(ano);
-                    articuloindexado.setEdoactual(edoactual);
+                    articuloindexado.setEstado(edoactual);
                     articuloindexado.setLgac(lgac);
                     articuloindexado.setValidadoCA(ca);                    
                     articuloindexado.registrarProducto();
                 }
             }
         }
+    
+    /**
+     * Metódo que recopila la información de los campos en la GUI para la creación
+     * de un objeto ARTICULO INDEXADO
+     */
+    private void guardarArticuloArbitrado(){
+        /**
+         * Recopilación de datos de campos de texto
+         */
+        ArticuloArbitrado articuloarbitrado = new ArticuloArbitrado();
+        String titulo = txttituloarbitrado.getText();
+        String autor = txtautorarbitrado.getText();
+        String nomrevista = txtnomrevistaarbitrado.getText();
+        String editorial = txteditorialarbitrado.getText();
+        String proposito = txtpropositoarbitrado.getText();
+        String issn = txtissnarbitrado.getText();
+        String direlec = txtdirelecarbitrado.getText();
+        String vol = txtvolarbitrado.getText();
+        String pags = txtpagsarbitrado.getText();
+        String pais = txtpaisarbitrado.getText();
+        String ano = txtanoarbitrado.getText();
+        String edoactual = txtedoactualarbitrado.getText();
+        String lgac = cbxlgacarbitrado.getSelectionModel().getSelectedItem();
+        boolean ca = chkcaarbitrado.isSelected();
+        /**
+         * Validación de campos vacíos
+         */
+        if(titulo.isEmpty() || autor.isEmpty() || nomrevista.isEmpty() || editorial.isEmpty()
+                || proposito.isEmpty() || issn.isEmpty() || direlec.isEmpty()||
+                vol.isEmpty() || pags.isEmpty() || pais.isEmpty() || ano.isEmpty()
+                || edoactual.isEmpty()){
+            System.err.println("Campos Vacíos");
+        }else{
+            if(lgac == null){
+                System.out.println("LGAC no seleccionada");
+            }else{
+                    articuloarbitrado.setTitulo(titulo);          
+                    articuloarbitrado.setAutor(autor); 
+                    articuloarbitrado.setNomrevista(nomrevista); 
+                    articuloarbitrado.setEditorial(editorial); 
+                    articuloarbitrado.setVolumen(vol);
+                    articuloarbitrado.setPags(pags);
+                    articuloarbitrado.setPais(pais);
+                    articuloarbitrado.setAno(ano);
+                    articuloarbitrado.setEstado(edoactual);
+                    articuloarbitrado.setLgac(lgac);
+                    articuloarbitrado.setValidadoCA(ca);                    
+                    articuloarbitrado.registrarProducto();
+                }
+            }
+        }
+    
+    /**
+     * Metódo que recopila la información de los campos en la GUI para la creación
+     * de un objeto ARTICULO ARBITRADO
+     */
+    private void guardarArticulo(){
+        /**
+         * Recopilación de datos de campos de texto
+         */
+        Articulo articulo = new Articulo();
+        String titulo = txttituloarticulo.getText();
+        String autor = txtautorarticulo.getText();
+        String nomrevista = txtnomrevistaarticulo.getText();
+        String editorial = txteditorialarticulo.getText();
+        String proposito = txtpropositoarticulo.getText();
+        String issn = txtissnarticulo.getText();
+        String vol = txtvolarticulo.getText();
+        String pags = txtpagsarticulo.getText();
+        String pais = txtpaisarticulo.getText();
+        String ano = txtanoarticulo.getText();
+        String edoactual = txtedoactualarticulo.getText();
+        String lgac = cbxlgacarticulo.getSelectionModel().getSelectedItem();
+        boolean ca = chkcaarticulo.isSelected();
+        /**
+         * Validación de campos vacíos
+         */
+        if(titulo.isEmpty() || autor.isEmpty() || nomrevista.isEmpty() || editorial.isEmpty()
+                || proposito.isEmpty() || issn.isEmpty() || vol.isEmpty() || 
+                pags.isEmpty() || pais.isEmpty() || ano.isEmpty()
+                || edoactual.isEmpty()){
+            System.err.println("Campos Vacíos");
+        }else{
+            if(lgac == null){
+                System.out.println("LGAC no seleccionada");
+            }else{
+                    articulo.setTitulo(titulo);          
+                    articulo.setAutor(autor); 
+                    articulo.setNomrevista(nomrevista); 
+                    articulo.setEditorial(editorial); 
+                    articulo.setVolumen(vol);
+                    articulo.setPaginas(pags);
+                    articulo.setPais(pais);
+                    articulo.setAno(ano);
+                    articulo.setEstado(edoactual);
+                    articulo.setLgac(lgac);
+                    articulo.setValidadoCA(ca);                    
+                    articulo.registrarProducto();
+                }
+            }
+        }
+    
+    /**
+     * Metódo que recopila la información de los campos en la GUI para la creación
+     * de un objeto PRODDUCCION INNOVADORA
+     */
+    private void guardarProduccionInnovadora(){
+        /**
+         * Recopilación de datos de campos de texto
+         */
+        ProduccionInnovadora produccioninnovadora = new ProduccionInnovadora();
+        String titulo = txttituloprod.getText();
+        String participante = txtparticipanteprod.getText();
+        String clasifinter = txtclasifprod.getText();
+        String numreg = txtnumregprod.getText();
+        LocalDate fecha = dtpfechaprod.getValue();
+        String proposito = txtpropositoindexado.getText();
+        String pais = txtpaisindexado.getText();
+        String edoactual = txtedoactualindexado.getText();
+        String lgac = cbxlgacindexado.getSelectionModel().getSelectedItem();
+        boolean ca = chkcaindexado.isSelected();
+        /**
+         * Validación de campos vacíos
+         */
+        if(titulo.isEmpty() || participante.isEmpty() || clasifinter.isEmpty() || numreg.isEmpty()
+                || proposito.isEmpty() || fecha == null){
+            System.err.println("Campos Vacíos");
+        }else{
+            if(lgac == null){
+                System.out.println("LGAC no seleccionada");
+            }else{
+                    produccioninnovadora.setTitulo(titulo);          
+                    produccioninnovadora.setParticipante(participante); 
+                    produccioninnovadora.setClasifinternacional(clasifinter); 
+                    produccioninnovadora.setNumregistro(numreg); 
+                    produccioninnovadora.setFechapub(fecha);
+                    produccioninnovadora.setPais(pais);
+                    produccioninnovadora.setEstado(edoactual);
+                    produccioninnovadora.setLgac(lgac);
+                    produccioninnovadora.setValidadoCA(ca);                    
+                    produccioninnovadora.registrarProducto();
+                }
+            }
+        }
+    
+    /**
+     * Metódo que recopila la información de los campos en la GUI para la creación
+     * de un objeto MEMORIA EN EXTENSO
+     */
+    private void guardarMemoriaExtenso(){
+        /**
+         * Recopilación de datos de campos de texto
+         */
+        MemoriaExtenso memoriaextenso = new MemoriaExtenso();
+        String titulo = txttitulopresmem.getText();
+        String autor = txtautormem.getText();
+        String congreso = txtcongresomem.getText();
+        String pags = txtpagsmem.getText();
+        String estado = txtedomem.getText();
+        String ciudad = txtciudadmem.getText();
+        String pais = txtpaismem.getText();
+        String ano = txtanomem.getText();
+        String proposito = txtapropositomem.getText();
+        String edoactual = txtedoactualmem.getText();
+        String lgac = cbxlgacmem.getSelectionModel().getSelectedItem();
+        boolean ca = chkcamem.isSelected();
+        /**
+         * Validación de campos vacíos
+         */
+        if(titulo.isEmpty() || autor.isEmpty() || congreso.isEmpty() || estado.isEmpty()
+                || ciudad.isEmpty() || pags.isEmpty() || pais.isEmpty() || ano.isEmpty()
+                || edoactual.isEmpty()){
+            System.err.println("Campos Vacíos");
+        }else{
+            if(lgac == null){
+                System.out.println("LGAC no seleccionada");
+            }else{
+                    memoriaextenso.setTitulo(titulo);          
+                    memoriaextenso.setAutor(autor);
+                    memoriaextenso.setCongreso(congreso);
+                    memoriaextenso.setRangopags(pags);
+                    memoriaextenso.setCiudad(ciudad);
+                    memoriaextenso.setEstado(estado);
+                    memoriaextenso.setPais(pais);
+                    memoriaextenso.setAno(ano);
+                    memoriaextenso.setProposito(proposito);
+                    memoriaextenso.setEstado(edoactual);
+                    memoriaextenso.setLgac(lgac);
+                    memoriaextenso.setValidadoCA(ca);                    
+                    memoriaextenso.registrarProducto();
+                }
+            }
+        }
     }
-}
+
+
